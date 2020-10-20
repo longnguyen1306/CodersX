a1c8f7a162d592082a830fb82655cec827a65c87
unknown <ldc.longnd@gmail.com>
2020-10-20 12:14:19 +0700

    bai 1

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function (request, response) {
+    response.send('<h1>Hello</h1>')
+});
+
+app.get('/users', function (request, response) {
+    response.send('User list')
+});
+
+app.listen(port, function () {
+    console.log('Server listening on port' + port)
+});
